d31647d7ea4268c1ef58321052d56adaac263e7c
vipinkrajput <vipin.hbti.in@gmail.com>
2024-02-17 20:55:24 -0800

    fix output format

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ greeting_message="Hello, My name is $1, I am working with $2"
 echo "${greeting_message}"
 time=$(date)
 echo "time=$time"
-echo "${greeting_message}" >> $GITHUB_OUTPUT
+echo "message=${greeting_message}" >> $GITHUB_OUTPUT
 
