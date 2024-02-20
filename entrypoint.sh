6d2a76c23923ecb9360e96cb438cc6beca48a17f
vipinkrajput <vipin.hbti.in@gmail.com>
2024-02-19 20:50:35 -0800

    Fix: Linter complain

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ greeting_message="Hello, My name is $1, I am working with $2"
 echo "${greeting_message}"
 time=$(date)
 echo "time=$time"
-echo "message=${greeting_message}" >> $GITHUB_OUTPUT
+echo "message=${greeting_message}" >> "$GITHUB_OUTPUT"
